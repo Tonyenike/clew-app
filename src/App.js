8990933a9f7b9d27b015990bfb2a394b2d0d02b4
Ben Chopson <bjamin513@gmail.com>
2018-04-14 17:42:29 +0000

    Navigation and landing page
    * Add navbar
    * Add nice-looking landing page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { ToastContainer } from 'react-toastify';
+import { Menu } from 'semantic-ui-react';
 import Main from './components/Main';
 import NewGame from './components/NewGame';
 import PlayGame from './components/PlayGame';
+import './App.css';
 
 class App extends Component {
   render() {
     const { store } = this.props;
     return (
-      <Container>
+      <Container className="below-header">
         <ToastContainer hideProgressBar />
-        <Link to="/">
-          <h1>Clew</h1>
-        </Link>
+        <Menu fixed="top">
+          <Link to="/">
+            <Menu.Item name="Home" />
+          </Link>
+          <Link to="/new-game">
+            <Menu.Item name="New Game" />
+          </Link>
+        </Menu>
         <Switch>
           <Route exact={true} path="/" render={() => <Main store={store} />} />
           <Route path="/new-game" render={() => <NewGame store={store} />} />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react';
+import { Button, Container } from 'semantic-ui-react';
+import './Main.css';
 
 @observer
 export default class Main extends Component {
   render() {
-    return <Link to="/new-game">New Game</Link>;
+    return (
+      <Container className="hero center aligned text">
+        <h1>Clew</h1>
+        <Link to="/new-game">
+          <Button size="massive" primary>
+            New Game
+          </Button>
+        </Link>
+        <Link to="/play">
+          <Button size="massive" secondary>
+            Resume Game
+          </Button>
+        </Link>
+      </Container>
+    );
   }
 }
